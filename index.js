41709316cf333eb2282ab17cbd2ba12340bac22f
johSD <joh.workonly@gmail.com>
2025-08-27 17:26:45 +0900

    update from old file

--- index.js
+++ index.js
@@ -8,28 +8,53 @@
 //   }, 1000);
 
 let homescoreEl = document.getElementById("homescore");
-
-let homescorebtn1 = document.getElementById("plus-1btn")
-
+//let homescorebtn1 = document.getElementById("plus-1btn")
 let homeScore= 0
 
-function plus1() {
+function plusHome1() {
     homeScore+=1
     homescoreEl.textContent=homeScore
 }
 
 let homescorebtn2 =document.getElementById("plus-2btn")
 
-function plus2() {
+function plusHome2() {
     homeScore+= 2
     homescoreEl.textContent=homeScore
 }
 
 let homescorebtn3 =document.getElementById("plus-3btn")
-function plus3(){
+function plusHome3(){
     homeScore+=3
     homescoreEl.textContent=homeScore
 }
 
+let guestscore = 0
+let guestscoreEl= document.getElementById("guestscore")
+
+function plusGuest1(){
+    guestscore+=1
+    guestscoreEl.textContent =guestscore
+
+}
+
+function plusGuest2(){
+    guestscore+=2
+    guestscoreEl.textContent =guestscore
+}
+
+function plusGuest3(){
+    guestscore+=3
+    guestscoreEl.textContent= guestscore
+}
+
+let newgameEl=document.getElementById ="newGame"
+
+function newGame(){
+    homescoreEl.textContent = 0
+    guestscoreEl.textContent =0
+    homeScore= 0
+    guestscore=0
+}
 
 
